4a029d376c079a3506b5848ae7b2d390159a10f0
Eiji MATSUMOTO <e.mattsan@gmail.com>
2018-04-21 10:59:30 +0900

    initial commit

---
+++ lib/proc_tree.ex
@@ -0,0 +1,18 @@
+defmodule ProcTree do
+  @moduledoc """
+  Documentation for ProcTree.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ProcTree.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/proc_tree_test.exs
@@ -0,0 +1,8 @@
+defmodule ProcTreeTest do
+  use ExUnit.Case
+  doctest ProcTree
+
+  test "greets the world" do
+    assert ProcTree.hello() == :world
+  end
+end
